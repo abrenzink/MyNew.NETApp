b1612218e66233af6068f7f4be96d4903494b8ed
abrenzink <abrenzink@bonadmin.com>
2025-11-05 20:31:44 -0300

    feat: endpoints filters

--- obj/Debug/net8.0/MyNewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyNewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106f1df5b617e6178667012eb58111a67ea7096b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
